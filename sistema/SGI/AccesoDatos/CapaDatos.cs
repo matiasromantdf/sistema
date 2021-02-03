94b4a9b760aa5061db68e81631bcaac51c0fc9e9
matiasromantdf <mroman.tdf@gmail.com>
2021-02-03 17:44:12 -0300

    separcion en capas

---
+++ sistema/SGI/AccesoDatos/CapaDatos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+namespace AccesoDatos
+{
+    public class Conexion
+    {
+       SqlConnection conectar= new SqlConnection("Data Source=NOTEBOOKPAPI\\SQLEXPRESS;" +
+                "Initial Catalog=ABM_complejo;" +
+                "Integrated Security = True" +
+                "");
+    }
+    public class Articulo
+    {
+        string cod_articulo;
+        int id_proveedor_articulos;
+        string descr_articulo;
+        float costo_articulo;
+        float precio_articulo;
+        float iva;
+        int stock_articulo;
+        int reposicion_articulo;
+        string unidad_medida;
+
+
+    }
+    public class Caja
+    {
+
+    }
+    public class Ventas
+    {
+
+    }
+
+}
